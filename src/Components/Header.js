720561b177eb023c88be73c34af2f5273281eeb6
Raul Alonzo Mendoza <ram2489@gmail.com>
2025-03-03 14:58:06 -0600

    Refactored Header.js to reduce code repetition and wrapped layout header components in the custom layout component also implemented responsive css media query rules for the 3 breakpoints used

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,22 +3,56 @@ import "../styles/landing.css";
 import "../styles/header.css";
 import "../styles/helpers.css";
 
+import HorizontalLayout from "./HorizontalLayout";
+
 function LittleLemonLogo() {
   return (
     <img
       className="outline"
-      src="/assets/Logo.svg"
+      src="/assets/images/Logo.svg"
       alt="Little Lemon Restaurant Logo"
     />
+  );
+}
+
+function SideMenu() {
+  return <img src="/assets/svgs/hamburger_menu.svg" alt="Hamburger icon" />;
+}
+
+function Basket() {
+  return (
+    <img src="/assets/svgs/basket.svg" alt="Basket icon for online delivery" />
+  );
+}
+
+function MobileHeader() {
+  return (
+    <>
+      <SideMenu />
+      <LittleLemonLogo />
+      <Basket />
+    </>
+  );
+}
+
+function DesktopHeader() {
+  return (
+    <>
+      <LittleLemonLogo />
+      <Nav />
+      <Basket />
+    </>
   );
 }
 
 export default function Header() {
   return (
     <header className="navigation-bar outline">
-      <div className="constrain-content outline">
-        <LittleLemonLogo />
-        <Nav />
+      <div className="constrain-content outline flex-nav">
+        <HorizontalLayout>
+          <DesktopHeader />
+          <MobileHeader />
+        </HorizontalLayout>
       </div>
     </header>
   );
