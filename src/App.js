d281560c432825dca985c9c56f7fa9f3620b6594
Raul Alonzo Mendoza <ram2489@gmail.com>
2025-03-05 09:35:25 -0600

    Import all common css files from root component
    All common css files used by the components will be imported from the App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import Header from "./Components/Header";
 import Main from "./Components/Main/Main";
 import Footer from "./Components/Footer";
+
 import "./styles/landing.css";
+import "./styles/helpers.css";
+import "./styles/fonts.css";
 
 function App() {
   return (
