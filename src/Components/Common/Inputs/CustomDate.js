83837455d0441b105f03b15226c4f8ad135b8b57
Raul Alonzo Mendoza <ram2489@gmail.com>
2025-03-20 17:24:06 -0500

    Centralize css rules for inputs
    To avoid duplicating css rules with different styles an input.css file was created that centralizes all rules into a single file and the rules were removed from the other files. The Components were updated with the new class names to avoid issues.

--- src/Components/Common/Inputs/CustomDate.js
+++ src/Components/Common/Inputs/CustomDate.js
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 
 import "../../../styles/fonts.css";
 import "../../../styles/inputs/custom-date.css";
+import "../../../styles/inputs/input.css";
 
 export default function CustomDate({
   name,
@@ -21,7 +22,7 @@ export default function CustomDate({
       ) {
         ref.current.showPicker();
       } else {
-        console.warn("shoPicker() is not supported by the browser");
+        console.warn("showPicker() is not supported by the browser");
       }
     } catch (error) {
       console.error("There was an error calling showPicker():", error);
@@ -42,19 +43,19 @@ export default function CustomDate({
   const minFormattedDate = (minDate || value).toISOString().slice(0, 10);
   const maxFormattedDate = maxDate.toISOString().slice(0, 10);
   return (
-    <div className="date-wrapper">
+    <div className="input-wrapper">
       {icon}
       <input
         name={name}
-        className="paragraph-text"
+        className="paragraph-text custom-input"
         type="date"
         ref={ref}
         value={curFormattedDate}
         onChange={handleOnChange}
-        min={minFormattedDate}
-        max={maxFormattedDate}
         onClick={handleOpenPicker}
         onKeyDown={handleKeyDown}
+        min={minFormattedDate}
+        max={maxFormattedDate}
       />
     </div>
   );
