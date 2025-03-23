a607cf0fbc15d556c5ae335627481cee5dfc648c
Raul Alonzo Mendoza <ram2489@gmail.com>
2025-03-23 11:45:24 -0500

    Add documentation and implement BaseInput as the Wrapper Component

--- src/Components/Common/Inputs/CustomDate.js
+++ src/Components/Common/Inputs/CustomDate.js
@@ -1,68 +1,66 @@
 import React, { useRef } from "react";
 
+import useOpenPicker from "../../../Hooks/useOpenPicker";
+import BaseInput from "./BaseInput";
+
 import "../../../styles/fonts.css";
 import "../../../styles/inputs/custom-date.css";
 import "../../../styles/inputs/input.css";
 
+/**
+ * Renders a Date picker input with a specific style and takes control of opening the picker from the HTML input via Ref.
+ *
+ * (All dates passed to the input need to follow the HTML Date string Format [https://developer.mozilla.org/en-US/docs/Web/HTML/Date_and_time_formats#date_strings]).
+ *
+ * @param {string} name - The name of the input.
+ * @param {string} [id] - The id of the input, if no id is provided it uses the name as the id.
+ * @param {string} [value] - The value passed to HTML input that updates what it displays.
+ * @param {Function(event)} [onChange] - The Callback function that will allow to read the event from the HTML input when its value changes.
+ * @param {Function(event)} [onBlur] - The Callback function that will allow to read the event from the HTML input when it looses focus.
+ * @param {string} [min] - The minimum date that can be selected on the input.
+ * @param {string} [max] - The maximum date that can be selected on the input.
+ * @param {React.Component} [icon] - An optional icon that is placed before the input element.
+ * @param {string} [className] - Additional CSS classes for the input.
+ * @param {string} [error] - The error message that will be displayed when the input is invalid.
+ * @param {boolean} [touched] - A flag that indicates if the input has been interacted with or not.
+ * @returns {React.Component} The custom Date picker input.
+ */
 export default function CustomDate({
   name,
   id,
-  onChangeEvent,
-  onBlur,
   value,
+  onChange,
+  onBlur,
   min,
   max,
   icon,
+  className,
   error,
   touched,
 }) {
   const ref = useRef(null);
-
-  function handleOpenPicker() {
-    try {
-      if (
-        "showPicker" in HTMLInputElement.prototype &&
-        ref.current?.showPicker
-      ) {
-        ref.current.showPicker();
-      } else {
-        console.warn("showPicker() is not supported by the browser");
-      }
-    } catch (error) {
-      console.error("There was an error calling showPicker():", error);
-    }
-  }
-
-  function handleKeyDown(event) {
-    if (event.key === "Enter" || event.key === " ") {
-      handleOpenPicker();
-    }
-  }
+  const { handleOpenPicker, handleKeyDown } = useOpenPicker(ref);
 
   function handleOnChange(event) {
-    onChangeEvent(event);
+    onChange(event);
   }
 
   return (
-    <div className="grid input-container-grid">
-      <div className="input-wrapper">
-        {icon}
-        <input
-          name={name}
-          id={id || name}
-          className="paragraph-text custom-input"
-          type="date"
-          ref={ref}
-          value={value}
-          onChange={handleOnChange}
-          onBlur={onBlur}
-          onClick={handleOpenPicker}
-          onKeyDown={handleKeyDown}
-          min={min}
-          max={max}
-        />
-      </div>
-      {touched && error && <p className="input-error">{error}</p>}
-    </div>
+    <BaseInput icon={icon} touched={touched} error={error}>
+      <input
+        name={name}
+        id={id || name}
+        className={`${"paragraph-text custom-input"} ${className}`}
+        type="date"
+        ref={ref}
+        value={value}
+        onChange={handleOnChange}
+        onBlur={onBlur}
+        onClick={handleOpenPicker}
+        onKeyDown={handleKeyDown}
+        min={min}
+        max={max}
+      />
+    </BaseInput>
   );
 }
