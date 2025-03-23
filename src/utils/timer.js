fc03095cfa6036b8b3430869e9c65f5424fbefae
Raul Alonzo Mendoza <ram2489@gmail.com>
2025-03-23 11:54:34 -0500

    Add missing documentation for modules and Hooks

--- src/utils/timer.js
+++ src/utils/timer.js
@@ -1,3 +1,8 @@
+/**
+ * Creates a void promise that resolves after x millisecons have passed to simulate a delay event.
+ * @param {int} ms - The millisecond that will pass before the promise resolves.
+ * @returns - The resolved promised.
+ */
 export function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
