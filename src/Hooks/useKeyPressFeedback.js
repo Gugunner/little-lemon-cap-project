19df24289bd6bf00b4ffd6625ccafd59be7d021a
Raul Alonzo Mendoza <ram2489@gmail.com>
2025-03-26 11:40:45 -0500

    Add documentation to useKeyPressFeedback Hook

--- src/Hooks/useKeyPressFeedback.js
+++ src/Hooks/useKeyPressFeedback.js
@@ -1,6 +1,13 @@
 import { useCallback, useRef } from "react";
 import { delay } from "../utils/time";
 
+/**
+ * A custom Hook that creates a callback that allows animating an element after a user press enter or space in the keyboard.
+ *
+ * @param {string} animation - The css keyframe animation class used by the element.
+ * @param {int} duration - The duration in ms where the element has the animation class.
+ * @returns
+ */
 export default function useKeyPressFeedback(
   animation = "animate-press",
   duration = 150
