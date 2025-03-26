7176b75ea1c017721cc90c20f9ae2e25659ba352
Raul Alonzo Mendoza <ram2489@gmail.com>
2025-03-26 08:04:23 -0500

    Used route nesting for ReserveTablePage Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import LandingPage from "./Components/LandingPage";
+import ReservationSection from "./Components/ReserveTable/ReservationSection";
 import ReserveTablePage from "./Components/ReserveTablePage";
 
-import { homePath, reserveTablePath } from "./Constants/paths";
+import {
+  confirmReservePath,
+  homePath,
+  reserveTablePath,
+} from "./Constants/paths";
 
+import ConfirmReserveSection from "./Components/ReserveTable/ConfirmReserveSection";
 import "./styles/fonts.css";
 import "./styles/helpers.css";
 import "./styles/landing.css";
@@ -13,7 +19,10 @@ function App() {
   return (
     <Routes>
       <Route path={homePath} element={<LandingPage />} />
-      <Route path={reserveTablePath} element={<ReserveTablePage />} />
+      <Route path={reserveTablePath} element={<ReserveTablePage />}>
+        <Route index element={<ReservationSection />} />
+        <Route path={confirmReservePath} element={<ConfirmReserveSection />} />
+      </Route>
     </Routes>
   );
 }
